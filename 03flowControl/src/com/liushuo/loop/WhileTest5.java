997d16896414b0940f179272d0b7764c4a0db4e0
cn_liushuo <cn_liushuo@qq.com>
2025-01-13 16:17:42 +0800

    feat(loop): 添加calc2函数，计算折叠次数

--- 03flowControl/src/com/liushuo/loop/WhileTest5.java
+++ 03flowControl/src/com/liushuo/loop/WhileTest5.java
@@ -4,6 +4,7 @@ public class WhileTest5 {
     public static void main(String[] args) {
         // 目标：完成while循环的需求：复利计算。
         System.out.println("需要多少年：" + calc());
+        System.out.println("需要折叠多少次：" + calc2());
     }
 
     // 复利计算：我们本金是10万，复利是1.7%，请问多少年后本金翻倍。
@@ -21,5 +22,23 @@ public class WhileTest5 {
         }
 
         return year;
+    }
+
+    // 需求：珠穆朗玛峰高度是8848860，纸张厚度是0.1，折叠多少次可以达到山峰高度。
+    public static int calc2() {
+        // 1、把数据拿到程序中来。
+        double height = 8848860;
+        double thickness = 0.1;
+        int count = 0;
+
+        // 2、控制循环，只要纸张厚度还没有达到珠穆朗玛峰的高度，就需要继续折叠。
+        while (thickness < height) {
+            // 3、计算折叠次数。
+            count++;
+            // 4、计算纸张厚度。
+            thickness *= 2; // thickness = thickness * 2;
+        }
+        // 5、返回折叠次数。
+        return count;
     }
 }
