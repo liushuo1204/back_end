450576394e3368a48299fb0cd7952da61a3e816b
cn_liushuo <cn_liushuo@qq.com>
2025-01-15 18:07:46 +0800

    feat(object): 在 Test2 类中添加对象相关注释，并完善学生对象的输出展示

--- day05oop/src/com/liushuo/object/Test2.java
+++ day05oop/src/com/liushuo/object/Test2.java
@@ -1,6 +1,12 @@
 package com.liushuo.object;
 
 public class Test2 {
+    /**
+     * 1、对象是啥？如何得到？
+     * 对象就是一种特殊的数据结构。对象使用类new出来的，有了类就可以创建出对象。
+     * 2、面向对象编程这种套路是咋回事？
+     * 万物皆对象，谁的数据谁存储
+     */
     public static void main(String[] args) {
         // 目标：创建学生对象存储学生数据，并操作学生数据
         Student s1 = new Student();
@@ -9,6 +15,7 @@ public class Test2 {
         s1.math = 100;
         s1.printAllScore();
         s1.printAverageScore();
+        System.out.println(s1);
 
         Student s2 = new Student();
         s2.name = "播仔";
@@ -16,5 +23,6 @@ public class Test2 {
         s2.math = 100;
         s2.printAllScore();
         s2.printAverageScore();
+        System.out.println(s2);
     }
 }
