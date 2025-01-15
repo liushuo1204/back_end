579d555b5a6b51ab56e1e91d7642b81dbe140a4e
cn_liushuo <cn_liushuo@qq.com>
2025-01-15 18:27:59 +0800

    feat(object): 新增 Student 类和 Test 类，实现学生对象的创建与构造器的应用展示

---
+++ day05oop/src/com/liushuo/constructor/Student.java
@@ -0,0 +1,24 @@
+package com.liushuo.constructor;
+
+public class Student {
+    String name;
+    int age;
+    char sex;
+
+    // 1、无参数构造器
+    // 构造器：是一种特殊方法，不能写返回值类型，名称必须是类名，就是构造器。
+    public Student() {
+        System.out.println("无参构造器执行了");
+    }
+
+    // 2、有参数构造器
+    public Student(String n) {
+        System.out.println("有String参数构造器执行了");
+    }
+
+    public Student(String n, int a, char s) {
+        name = n;
+        age = a;
+        sex = s;
+    }
+}
---
+++ day05oop/src/com/liushuo/constructor/Test.java
@@ -0,0 +1,25 @@
+package com.liushuo.constructor;
+
+public class Test {
+    public static void main(String[] args) {
+        // 目标：认识类的构造器，搞清楚其特点和常见应用场景。
+        // 构造器的特点：创建对象时，对象会立即自动调用构造器。
+        Student s1 = new Student();
+        Student s2 = new Student("西门吹雪");
+
+        System.out.println("==========");
+        Student t1 = new Student();
+        t1.name = "石头";
+        t1.age = 22;
+        t1.sex = '男';
+        System.out.println(t1.name);
+        System.out.println(t1.age);
+        System.out.println(t1.sex);
+
+        // 对象的一种常见应用场景，创建对象时可以立即为对象赋值。
+        Student t2 = new Student("小河", 18, '女');
+        System.out.println(t2.name);
+        System.out.println(t2.age);
+        System.out.println(t2.sex);
+    }
+}
